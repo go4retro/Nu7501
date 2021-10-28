03cd4c0e1159f417c6d096f0e3c267934057c5f0
Mathieu Donze <mathieu.donze@cern.ch>
2021-10-28 18:17:17 +0200

    New way to handle GATE_IN, no more high RAM errors!

--- hdl/Fake7501.v
+++ hdl/Fake7501.v
@@ -19,8 +19,6 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
-//`define LATCH
-
 module Fake7501(input _reset,
                 input clock,
                 input r_w_6502,
@@ -44,8 +42,8 @@ wire ce_0000 =           ce_pio & !address_6502[0];
 wire ce_0001 =           ce_pio & address_6502[0];
 
 assign address_7501 =      (aec ? address_6502 : 16'bz);
-assign data_6502 =         data_6502_out;
-assign data_7501 =         data_7501_out;
+assign data_6502 =          data_6502_out;
+assign data_7501 =          data_7501_out;
 assign pio[6] =            (ddr_pio[6] ? data_pio[6] : 'bz);
 assign pio[5] =            (ddr_pio[5] ? data_pio[5] : 'bz);
 assign pio[4] =            (ddr_pio[4] ? data_pio[4] : 'bz);
@@ -54,70 +52,69 @@ assign pio[2] =            (ddr_pio[2] ? data_pio[2] : 'bz);
 assign pio[1] =            (ddr_pio[1] ? data_pio[1] : 'bz);
 assign pio[0] =            (ddr_pio[0] ? data_pio[0] : 'bz);
 
-assign r_w_7501 = (aec ? (ce_pio ? 'bz : r_w_latched) : 'bz);
+assign r_w_7501 = (aec ? (r_w_latched ? 'bz : r_w_6502) : 'bz);
 
-//Latch r/w to not update it while gate_in
-always @(r_w_6502)
+always @(posedge gate_in)
 begin
-	if(gate_in)
-	begin
-		r_w_latched <= r_w_6502;
-	end
+	if(!aec)
+		begin
+			r_w_latched = 'b1;
+		end
+	else
+		begin
+			r_w_latched = 'b0;
+		end
 end
 
 always @(*)
 begin
-   if(aec & !ce_pio &!r_w_latched) // write cycle
-   begin
-      data_7501_out = data_6502;
-      data_6502_out = 8'bz;
-   end
-	else if(aec & ce_pio &!r_w_latched) // write cycle to PIO
-   begin
-      data_7501_out = 8'bz;	//Do not write to peripherials
-      data_6502_out = 8'bz;
-   end
-	else if (aec & ce_0000 & r_w_latched) // read PIO DDR
-   begin
-      data_7501_out = 8'bz;
-      data_6502_out = {ddr_pio[6:5], 1'b0, ddr_pio[4:0]};
-   end
-   else if (aec & ce_0001 & r_w_latched) // read PIO value
-   begin
-      data_7501_out = 8'bz;
-      data_6502_out = {pio[6:5], 1'b0, pio[4:0]};
-   end
-	else if (aec & r_w_latched) // read cycle
-   begin
-      data_7501_out = 8'bz;
-      data_6502_out = data_7501;
-   end
+	if(!aec)
+		begin
+			data_7501_out = 8'bz;
+			data_6502_out = 8'bz;
+		end
+	else if (ce_0000 & r_w_6502) // read PIO DDR
+		begin
+			data_7501_out = 8'bz;
+			data_6502_out = {ddr_pio[6:5], 1'b0, ddr_pio[4:0]};
+		end
+	else if (ce_0001 & r_w_6502) // read PIO value
+		begin
+			data_7501_out = 8'bz;
+			data_6502_out = {pio[6:5], 1'b0, pio[4:0]};
+		end
+	else if(r_w_6502)
+	//Read from outside
+		begin
+			data_7501_out = 8'bz;
+			data_6502_out = data_7501;
+		end
 	else
-	begin
-		//aec active
-		data_7501_out = 8'bz;
-		data_6502_out = 8'bz;
-	end
+	//Write to outside
+		begin
+			data_7501_out = data_6502;
+			data_6502_out = 8'bz;
+		end 
 end
 
 //PIO write 
 always @(negedge clock, negedge _reset)
 begin
    if(!_reset)
-   begin
-      ddr_pio <= 0;
-      data_pio <= 0;
-   end
+		begin
+			ddr_pio <= 0;
+			data_pio <= 0;
+		end
    else if(!r_w_6502 & ce_0000)
-   begin
-		//Write PIO direction
-      ddr_pio <= {data_6502[7:6],data_6502[4:0]};
-   end
+		begin
+			//Write PIO direction
+			ddr_pio <= {data_6502[7:6],data_6502[4:0]};
+		end
    else if(!r_w_6502 & ce_0001)
-   begin
-		//Write data to PIO
-      data_pio <= {data_6502[7:6],data_6502[4:0]};
-   end
+		begin
+			//Write data to PIO
+			data_pio <= {data_6502[7:6],data_6502[4:0]};
+		end
 end
 
 endmodule
